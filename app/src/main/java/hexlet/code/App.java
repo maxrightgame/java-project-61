8e470a4095b772a80a2d48e60fd7d403116cc7f1
Max <maxrightgame@gmail.com>
2024-05-02 14:48:10 +0600

    Add base logic for Even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,27 @@
 package hexlet.code;
 
-import static hexlet.code.Cli.greeting; // непонятно почему Idea предлагает именно так и никак иначе
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        greeting();
+        System.out.println("Please enter the action number to start.");
+        System.out.println("1 - Greeting");
+        System.out.println("2 - Even numbers");
+        System.out.println("0 - Exit");
+        Scanner gameScanner = new Scanner(System.in);
+        int choice = Integer.parseInt(gameScanner.next());
+        switch (choice) {
+            case (0):
+                break;
+            case (1):
+                System.out.println("Welcome to the Brain Games!\nWhat do I call you?");
+                Scanner nameScanner = new Scanner(System.in);
+                String playerName = nameScanner.next();
+                System.out.println("Hello, " + playerName + "!");
+                break;
+            default:
+                System.out.println("Incorrect input!");
+                break;
+        }
     }
 }
