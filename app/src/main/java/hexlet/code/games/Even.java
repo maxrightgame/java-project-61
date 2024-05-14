edffca084b4cbb2039f96c8193c457dfec5ba848
maxrightgame <maxrightgame@gmail.com>
2024-05-14 17:46:14 +0600

    Fix git merge error

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -8,11 +8,7 @@ public class Even {
     public static final String RULES = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
 
     public static void evenGame() {
-<<<<<<< HEAD
-        Engine.printRules(RULES);
-=======
         System.out.println(RULES);
->>>>>>> 4276972c7f1930512e6639020a0f3cf3e64da505
         while (!Engine.checkWinStatus()) {
             int questionNumber = Engine.evenGameGenerateNumber();
             Engine.printQuestion(questionNumber);
